81b2bd5105bc3bb34e9124f0d5c927ebb0b09f3b
Christof Meerwald <cmeerw@users.noreply.github.com>
2011-09-11 09:08:39 +0000

    updated startup script

--- jabrss.sh
+++ jabrss.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 cd "`dirname $0`"
 exec python jabrssng.py \
-    --server=cmeerw.net \
-    --username=jabrss \
-    --connect-host=c2s.swissjabber.ch \
+    --jid=JabRSS@cmeerw.net/JabRSS --connect-host=swissjabber.ch \
     --password-file=/home/cmeerw/jabrss/jabrss-passwd
